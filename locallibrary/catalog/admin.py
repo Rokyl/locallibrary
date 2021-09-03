9be0fbc9fd0e1ffb16dc4c614b33e74efbf33e14
Rokyl <Shidlovskiy-02@mail.ru>
2021-09-03 15:12:35 +0300

    1

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display =  ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+class BookAdmin(admin.ModelAdmin):
+    pass
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Author,AuthorAdmin)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Book)
